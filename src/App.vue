520b362cd6a36bcfccea62faa9cdf3e57ca8ddf7
RaiJ1n <129413551+RaiJ1n@users.noreply.github.com>
2024-06-20 16:42:40 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,14 @@
   <div class="app-background">
     <h1 class="text-center text-2xl font-bold mb-4">Todo Vue</h1>
     <p class="mb-4">Number of Tasks: {{ listOfTask.length }}</p>
-    <div class="mb-4">
-      <p>{{ taskInput }}</p>
-    </div>
-    <ul class="space-y-2">
+    <ul>
       <div v-if="checkTask">
-        <li v-for="task in listOfTask" :key="task.id" class="flex justify-between items-center p-2 rounded"
-          :class="{ 'bg-green-400 text-white': task.isCompleted, 'bg-red-600 text-white': !task.isCompleted }">
+        <li v-for="task in listOfTask" :key="task.id" class="flex justify-between items-center p-2 rounded mb-2"
+          :class="{ 'bg-blue-600 text-white': task.isCompleted, 'bg-red-600 text-black': !task.isCompleted }">
           <div>{{ task.name }}</div>
           <div class="flex items-center">
             <input type="checkbox" v-model="task.isCompleted" class="mr-2">
-            <button v-on:click="deleteTask(task.id)" class="bg-red-600 px-2 py-1 rounded ml-2" v-if="task.isCompleted">Remove</button>
+            <button v-on:click="deleteTask(task.id)" class="bg-gray-200 px-2 py-1 rounded ml-2" v-if="task.isCompleted">Remove</button>
           </div>
         </li>
       </div>
